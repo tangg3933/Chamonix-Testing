6301ff43a067a34a6fce9e802f7e35c2609a6e77
Gordon <tangy3933@gmail.com>
2022-06-17 13:13:39 +0930

    Added in US-05 DRR

--- SeleniumTest/SeleniumTest/UnitTest1.cs
+++ SeleniumTest/SeleniumTest/UnitTest1.cs
@@ -49,9 +49,42 @@ namespace Selenium_Demo
             Assert.AreEqual("underline solid rgb(35, 82, 124)", underline);
 
             System.Threading.Thread.Sleep(6000);
-
-            Console.WriteLine("Test Passed");
         }
+
+        [Test]
+        public void US05_AC01_AC02_DDR()
+        {
+            driver.Url = test_url;
+
+            // Login into OneBiosecurity Account
+            IWebElement login = driver.FindElement(By.Name("p$lt$ctl03$btnLogin$btnElem$btn"));
+            login.Click();
+            IWebElement EmailAdd = driver.FindElement(By.Name("Ecom_User_ID"));
+            EmailAdd.SendKeys("gordon.tang@chamonix.com.au");
+            IWebElement password = driver.FindElement(By.Name("Ecom_Password"));
+            password.SendKeys("CsmdstuoA33!");
+            driver.FindElement(By.ClassName("checkmark")).Click();
+            driver.FindElement(By.Name("search")).Click();
+
+            // View DDR questionnaire
+            driver.FindElement(By.LinkText("Chamonix Test")).Click();
+            IWebElement ViewResults = driver.FindElement(By.Name("p$lt$ctl05$pageplaceholder$p$lt$ctl05$pageplaceholder$p$lt$ctl01$DrrSummaryWebPart_for_Pestivirus$btnViewResults"));
+            ViewResults.Click();
+            driver.FindElement(By.LinkText("LIVESTOCK INTRODUCTIONS AND MANAGEMENT PRACTICES")).Click();
+
+            // Test for contents table on page and if the page title is highlighted on the table
+            String ContentsTable = driver.FindElement(By.Id("SideBarList")).GetAttribute("title");
+            String fontWeight = driver.FindElement(By.ClassName("currentStep")).GetCssValue("font-weight");
+            String colour = driver.FindElement(By.ClassName("currentStep")).GetCssValue("color");
+            String underline = driver.FindElement(By.ClassName("currentStep")).GetCssValue("text-decoration");
+            Assert.AreEqual("Table of Contents", ContentsTable);
+            Assert.AreEqual("700", fontWeight);
+            Assert.AreEqual("rgba(35, 82, 124, 1)", colour);
+            Assert.AreEqual("underline solid rgb(35, 82, 124)", underline);
+
+            System.Threading.Thread.Sleep(6000);
+        }
+
 
         [TearDown]
         public void close_Browser()
