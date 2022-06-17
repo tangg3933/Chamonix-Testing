c473b07a2c72abbf10e0fba551d91b53fc099acb
Gordon <tangy3933@gmail.com>
2022-06-17 11:23:43 +0930

    Updated Selenium Test. US-05 for BSR test case included.

--- SeleniumTest/SeleniumTest/UnitTest1.cs
+++ SeleniumTest/SeleniumTest/UnitTest1.cs
@@ -1,13 +1,11 @@
-using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System;
 
 namespace Selenium_Demo
 {
     class Selenium_Demo
     {
-        String test_url = "https://www.google.com";
+        String test_url = "https://1bisdevcms.azurewebsites.net/Home";
 
         IWebDriver driver;
 
@@ -20,15 +18,35 @@ namespace Selenium_Demo
         }
 
         [Test]
-        public void test_search()
+        public void US05_AC01_AC02_BSR()
         {
             driver.Url = test_url;
 
-            System.Threading.Thread.Sleep(2000);
+            // Login into OneBiosecurity Account
+            IWebElement login = driver.FindElement(By.Name("p$lt$ctl03$btnLogin$btnElem$btn"));
+            login.Click();
+            IWebElement EmailAdd= driver.FindElement(By.Name("Ecom_User_ID"));
+            EmailAdd.SendKeys("gordon.tang@chamonix.com.au");
+            IWebElement password = driver.FindElement(By.Name("Ecom_Password"));
+            password.SendKeys("CsmdstuoA33!");
+            driver.FindElement(By.ClassName("checkmark")).Click();
+            driver.FindElement(By.Name("search")).Click();
 
-            IWebElement searchText = driver.FindElement(By.Name("q"));
-            searchText.SendKeys("Facebook");
-            searchText.Submit();
+            // View BSR questionnaire
+            driver.FindElement(By.LinkText("Chamonix Test")).Click();
+            IWebElement ViewResults = driver.FindElement(By.Name("p$lt$ctl05$pageplaceholder$p$lt$ctl05$pageplaceholder$p$lt$ctl01$BSRSummary$btnViewResults"));
+            ViewResults.Click();
+            driver.FindElement(By.LinkText("PLANNING AND TRACEABILITY")).Click();
+
+            // Test for contents table on page and if the page title is highlighted on the table
+            String ContentsTable = driver.FindElement(By.Id("SideBarList")).GetAttribute("title");
+            String fontWeight = driver.FindElement(By.ClassName("currentStep")).GetCssValue("font-weight");
+            String colour = driver.FindElement(By.ClassName("currentStep")).GetCssValue("color");
+            String underline = driver.FindElement(By.ClassName("currentStep")).GetCssValue("text-decoration");
+            Assert.AreEqual("Table of Contents", ContentsTable);
+            Assert.AreEqual("700", fontWeight);
+            Assert.AreEqual("rgba(35, 82, 124, 1)", colour);
+            Assert.AreEqual("underline solid rgb(35, 82, 124)", underline);
 
             System.Threading.Thread.Sleep(6000);
 
